f9e57d43c6f4da2509b31ecc8e844d3912e7ea0e
Cesar Flores <136465@udlondres.com>
2026-04-27 18:07:54 -0600

    Inicio de proyecto
    Inicio de proyecto

---
+++ src/app/encabezado/encabezado.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encabezado',
+  standalone: true,
+  templateUrl: './encabezado.components.html',
+  styleUrl: './encabezado.components.css',
+})
+export class EncabezadoComponent {}
